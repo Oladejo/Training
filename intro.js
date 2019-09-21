d90c3f58cc9a34fd2b7413d7aca14c6b1b89777e
Oladejo <bab.oladejo@student.oauife.edu.ng>
2019-09-21 09:55:39 +0100

    remove comment

--- intro.js
+++ intro.js
@@ -1,13 +1,13 @@
 const nemo = ['nemo'];
 
 function findNemo(array){
-    let t0 = Date.now(); //performance.now();
+    let t0 = Date.now(); 
     for(let i = 0; i < array.length; i++){
         if(array[i] === 'nemo'){
             console.log('Found Nemo');
         }
     }
-    let t1 = Date.now(); //performance.now();
+    let t1 = Date.now(); 
     console.log('call time ' + (t1 - t0) + ' milliseconds')
 }
 
