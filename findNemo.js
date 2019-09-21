fa3216ff9e6a6fbeb896f475307410abaf15a07c
Oladejo <bab.oladejo@student.oauife.edu.ng>
2019-09-21 08:59:41 +0100

    newfile

---
+++ findNemo.js
@@ -0,0 +1,14 @@
+const nemo = ['nemo'];
+
+function findNemo(array){
+    let t0 = Date.now(); //performance.now();
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === 'nemo'){
+            console.log('Found Nemo');
+        }
+    }
+    let t1 = Date.now(); //performance.now();
+    console.log('call time ' + (t1 - t0) + ' milliseconds')
+}
+
+findNemo(nemo);
